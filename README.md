ede1baa7a11012907ad10d8bf76356d58485b8c9
mrmckeb <mrmckeb@users.noreply.github.com>
2024-11-26 13:46:04 +1100

    Add repro

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pw-repro
+
+If not using VS Code, ensure that your IDE is using the workspace TypeScript
+version.
+
+To see the reproduction in the terminal.
+
+```sh
+# Optional, if pnpm not installed.
+corepack enable
+
+# Install packages
+pnpm i
+
+# Run TypeCheck
+pnpm typecheck
+```
+
+Open `tests/example.spec.ts` to see this in your IDE.
---
+++ tests/example.spec.ts
@@ -0,0 +1,27 @@
+import { test as base, expect } from "@playwright/test";
+
+const test = base.extend({
+  page: async ({ baseURL, page }, use) => {
+    await page.goto(baseURL);
+    await use(page);
+  },
+});
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
